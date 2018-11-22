802349b5a936f95499dc23cc48e035addf8d132e
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-22 13:50:36 +0300

    Add ft_strlen

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 15:55:20 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/22 13:21:32 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/22 13:48:30 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,5 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
+size_t	ft_strlen(const char *s);
 #endif
