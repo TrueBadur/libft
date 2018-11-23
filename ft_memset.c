bce50ecb67e0a1485904724eaa164fafd6c2fa11
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-23 23:55:28 +0300

    Fix ft_memset : refactor and optimise

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 16:06:41 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/20 17:29:18 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/23 23:12:22 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*b_m;
-	size_t			i;
+	size_t	i;
 
-	b_m = (unsigned char *)b;
 	i = 0;
 	while (i < len)
-		b_m[i++] = (unsigned char)c;
+		((unsigned char *)b)[i++] = (unsigned char)c;
 	return (b);
 }
