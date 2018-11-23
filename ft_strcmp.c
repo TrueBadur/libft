5307a89a7ff28205db2a006b6ffc4674f15bdbbf
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-24 00:49:49 +0300

    Fix ft_strcmp ft_strncmp: make fater a little bit and add casting to unsigned char in return

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,16 +6,16 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 04:55:20 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/23 04:55:58 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/24 00:48:05 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 != 0 && *s2 != 0 && *s1 == *s2)
+	while (*s1 != 0 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
