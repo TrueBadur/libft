fb826aeca1c0adbd9a2f4378f2c484bc0876f03b
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-24 02:40:12 +0300

    Fix ft_memccpy: fix last symbol problem

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 22:22:28 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/22 23:15:48 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/23 20:50:27 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memccpy(void *dst, void const *src, int c, size_t n)
 
 	i = 0;
 	while ((size_t)++i <= n && ((unsigned char*)src)[i - 1] != (unsigned char)c)
+		((unsigned char *)dst)[i - 1] = ((unsigned char *)src)[i - 1];
+	if (((unsigned char*)src)[i - 1] == (unsigned char)c)
 		((unsigned char *)dst)[i - 1] = ((unsigned char *)src)[i - 1];
 	if (((unsigned char*)src)[i - 1] == (unsigned char)c)
 		return (dst + i);
