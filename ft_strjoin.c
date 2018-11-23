78d00d4fed62f65e977a10c82bb43e68bc763e54
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-23 09:13:26 +0300

    Add ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/23 08:11:14 by ehugh-be          #+#    #+#             */
+/*   Updated: 2018/11/23 08:25:12 by ehugh-be         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+	int		i;
+	int		j;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	if (!(ret = malloc(i + j + 1)))
+		return (NULL);
+	ft_strncpy(ret, s1, i + 1);
+	ft_strlcat(ret, s2, i + j + 1);
+	return (ret);
+}
