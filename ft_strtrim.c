8a6408e9883a8c41458d3d57cbddecb0af1ef6eb
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-23 23:53:57 +0300

    Fix ft_strtrim : error with all spaces and other

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 08:58:34 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/23 22:36:14 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/23 23:52:20 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 char	*ft_strtrim(char const *s)
 {
-	unsigned int	start;
-	size_t			len;
+	size_t	len;
 
 	if (!s)
 		return (NULL);
-	start = 0;
-	while (*(s + start) == ' ' || *(s + start) == '\n' || *(s + start) == '\t')
-		start++;
+	while (*s && (*s == ' ' || *s == '\n' || *s == '\t'))
+		s++;
 	len = ft_strlen(s);
-	while (*(s + len) == ' ' || *(s + len) == '\n' || *(s + len) == '\t')
+	while (len > 0 && (*(s + len) == ' ' || *(s + len) == '\0' || *(s + len) ==
+				'\n' || *(s + len) == '\t'))
 		len--;
-	return (ft_strsub(s, start, len));
+	if (len <= 0)
+		return (ft_strnew(1));
+	return (ft_strsub(s, 0, len + 1));
 }
