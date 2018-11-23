7ace55ebcd80b4bd60d1c0351223ff45aabb3ff0
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-23 05:05:42 +0300

    Add ft_strncmp.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 15:55:20 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/23 04:56:37 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/23 05:03:17 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,4 +55,5 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 #endif
