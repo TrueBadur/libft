6fb759c996cb1022b3d826f027fba05a8c406f53
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-11-27 19:47:00 +0300

    Fix ft_strlcpy: correspond ma declaration

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,25 +6,15 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 22:54:24 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/10/23 23:15:37 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/27 19:35:25 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				ft_strlen(char *str)
-{
-	int len;
+#include "libft.h"
 
-	len = 0;
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	char *src_c;
+	const char *src_c;
 
 	src_c = src;
 	while (*src && size)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 15:55:20 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/11/26 16:31:58 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/11/27 19:37:35 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,4 +83,5 @@ void				ft_lstaddlast(t_list **alst, t_list *new);
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 #endif
