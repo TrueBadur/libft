fa9784950afac817feca2e91b26bb07fbf1850b2
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-04-02 00:22:05 +0300

    Fix ft_atoi: add ft_atoi_safe; add function discriptions both to the ft_atoi and to ft_atoi_safe.

--- std_lib/ft_atoi.c
+++ std_lib/ft_atoi.c
@@ -11,6 +11,17 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+
+/*
+** @param str - string to try to convert to int
+** @return - int corresponding to one in string or 0 if something went wrong
+**
+** Skips space characters then while characters in string is numerical make int.
+** Once non numerical symbol is reached stops and returning current value.
+** If first nonspace character isn't numerical returns 0;
+** In case of int overflow behavior is undefined.
+*/
 
 int	ft_atoi(const char *str)
 {
@@ -38,4 +49,40 @@ int	ft_atoi(const char *str)
 		str++;
 	}
 	return (answer * pos);
+}
+
+/*
+** @param str - string to convert to int
+** @param ans - pointer to integer where to store converted int
+** @return - returns -1 if int overflow or int underflow had happened, 1 if
+ * nonnumeric symbol was reached and 0 if only numerical symbols were read.
+**
+** Skips space characters then while characters in string is numerical make int.
+** Once non numerical symbol or end is reached stops and returning current
+** 		value.
+ */
+
+char	ft_atoi_safe(const char *str, int *ans)
+{
+	long			answer;
+	char			pos;
+	unsigned char	d;
+
+	answer = 0;
+	pos = 1;
+	while (ft_isspace(*str))
+		str++;
+	str += (*str == '-') &&	(pos = -1) ? 1 : 0;
+	str += (*str == '+') ? 1 : 0;
+	while (1)
+	{
+		if ((d = *str - '0') > 9)
+			break ;
+		if (answer * 10 > INT_MAX)
+			return ((*ans = 0) - 1);
+		answer = answer * 10 + d;
+		str++;
+	}
+	*ans = answer * pos;
+	return (*str);
 }
