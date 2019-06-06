ae51b12031595638ee780b3f425a7b11a5f6eb85
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-06-07 02:30:21 +0300

    Fix crucial error in ft_vecbinsearch.c

--- std_lib/libvector/ft_vecbinsearch.c
+++ std_lib/libvector/ft_vecbinsearch.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vecbinsearch.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ehugh-be <ehugh-be@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 19:33:00 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/04/16 19:33:00 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/06/06 23:27:01 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,25 @@ int	ft_vecbinsearch_int(t_vector *vec, int val)
 {
 	int ret;
 	int tmp;
-	int mod;
+	int st;
+	int end;
 
-	ret = vec->len / sizeof(int) / 2 - 1;
-	mod = (vec->len / sizeof(int) - ret) / 2;
-	while (ret && (unsigned)ret != vec->len)
+	st = 0;
+	end = (vec->len / sizeof(int)) - 1;
+	while (end - st > 1)
 	{
+		ret = (st + end) / 2;
 		tmp = ((int*)vec->data)[ret];
 		if (val == tmp)
-			break ;
+			return (ret);
 		else if (val < tmp)
-			ret -= mod ? mod : 1;
+			end = ret;
 		else
-			ret += mod ? mod : 1;
-		mod /= 2;
+			st = ret;
 	}
-	if (ret == 0)
-		ret = ((int*)vec->data)[ret] == val ? ret : -1;
-	return (ret);
+	if (((int*)vec->data)[st] == val)
+		return (st);
+	else if (((int*)vec->data)[end] == val)
+		return (end);
+	return (-1);
 }
