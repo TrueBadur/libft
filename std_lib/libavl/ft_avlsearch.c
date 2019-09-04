cf046c33fd0ad745190b3d28bc0fe2b4f8786a92
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-09-04 20:21:06 +0300

    Post merge fixes

--- std_lib/libavl/ft_avlsearch.c
+++ std_lib/libavl/ft_avlsearch.c
@@ -30,7 +30,6 @@ t_btavl	*ft_avlmax(t_btavl *tr)
 /*
 ** @param tr - tree to search through
 ** @param key - what key to find in tree
-<<<<<<< HEAD
 ** @param ret_d - specifies what to return: data(1) or node(0) associated with
 ** 						key
 ** @return - if key is found then return ret_d associated with this key, NULL
@@ -50,21 +49,4 @@ void	*ft_avlsearch(t_btavl *tr, void *key, int ret_d, int (f)(void*, void*))
 		return (ft_avlsearch(tr->left, key, ret_d, f));
 	else
 		return (ft_avlsearch(tr->right, key, ret_d, f));
-=======
-** @param ret_d - specifies what to return: data or node assosiated with key
-** @return - if key is found then return ret_d assosiated with this key, NULL
-** otherwise
-*/
-
-void	*ft_avlsearch(t_btavl *tr, int key, int ret_d)
-{
-	if (!tr)
-		return (NULL);
-	if (tr->key == key)
-		return (ret_d ? tr->data : tr);
-	if (key < tr->key)
-		return (ft_avlsearch(tr->left, key, ret_d));
-	else
-		return (ft_avlsearch(tr->right, key, ret_d));
->>>>>>> 3978018af4a6e5d449ec5c0918d9cf37d4c6dba1
 }
